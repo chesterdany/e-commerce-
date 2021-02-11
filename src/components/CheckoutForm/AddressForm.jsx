1ff5989af691dc4dbf2d4ef2bdf40d5bd8424156
chesterdany <chesterdany22@yahoo.com>
2021-02-11 15:11:49 +0200

    basic shipping address form

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import {
+	InputLabel,
+	Select,
+	MenuItem,
+	Button,
+	Grid,
+	Typography,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+
+import CustomTextField from "./CustomTextField";
 
 const AddressForm = () => {
-  return <div>AddressForm</div>;
+	const methods = useForm();
+
+	return (
+		<>
+			<Typography variant="h6" gutterBottom>
+				Shipping Address
+			</Typography>
+			<FormProvider {...methods}>
+				<form onSubmit="">
+					<Grid container spacing={3}>
+						<CustomTextField
+							required
+							name="FirstName"
+							label="First Name"
+						/>
+						<CustomTextField
+							required
+							name="LastName"
+							label="Last Name"
+						/>
+						<CustomTextField
+							required
+							name="address1"
+							label="Address"
+						/>
+						<CustomTextField required name="email" label="Email" />
+						<CustomTextField required name="city" label="City" />
+						<CustomTextField
+							required
+							name="Zip"
+							label="ZIP / Postal Code"
+						/>
+					</Grid>
+				</form>
+			</FormProvider>
+		</>
+	);
 };
 
 export default AddressForm;
