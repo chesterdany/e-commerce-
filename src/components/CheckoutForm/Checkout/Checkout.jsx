2c69887e2e3a4faccf87b2a451db05ffc7a939fe
Brendea Daniel <chesterdany22@yahoo.com>
2021-03-02 17:04:26 +0200

    payment form added

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -52,7 +52,11 @@ const Checkout = ({ cart }) => {
 		activeStep === 0 ? (
 			<AddressForm checkoutToken={checkoutToken} next={next} />
 		) : (
-			<PaymentForm shippingData={shippingData} />
+			<PaymentForm
+				shippingData={shippingData}
+				checkoutToken={checkoutToken}
+				backStep={backStep}
+			/>
 		);
 
 	return (
